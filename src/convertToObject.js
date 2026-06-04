87c71b9607cf0a458c3d0c7e51d71f6881b69ad3
Adel Pereira <nic.gis.office@gmail.com>
2026-06-04 13:50:26 +0100

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styleObject = {};
+  const pairSplit = sourceString.split(';');
+
+  for (const pair of pairSplit) {
+    const [key, value] = pair.split(':');
+
+    if (value === undefined) {
+      continue;
+    }
+
+    styleObject[key.trim()] = value.trim();
+  }
+
+  return styleObject;
 }
 
 module.exports = convertToObject;
